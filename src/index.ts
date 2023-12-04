a52b8f6a1a7b4c4285c0443f506228cf844bc42a
ing.dacastillop <ing.dacastillop@gmail.com>
2023-12-04 13:13:38 -0500

    :sparkles: New funcion subscriber for lister change Language

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,19 @@ export type I18nTranslate<T extends I18nValue = I18nValue> = (
 
 let i18nLanguageGlobal = 'es';
 
+type I18nSubscriber = (language: string) => void;
+
+const subscribers: I18nSubscriber[] = [];
+
 /* istanbul ignore next */
 export function i18nLanguage(language: string): void {
   i18nLanguageGlobal = language;
+
+  subscribers.forEach((subscriber) => subscriber(language));
+}
+
+export function i18nSubscribe(subscriber: I18nSubscriber): void {
+  subscribers.push(subscriber);
 }
 
 export function i18n<T extends I18nValue = I18nValue>(
