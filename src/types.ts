39c73144b9596049e04d1c72f1f1f115b97178fb
ing.dacastillop <ing.dacastillop@gmail.com>
2024-11-16 09:38:34 -0500

    :bento: Add portuguese code for language

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { Interpolators } from '@rolster/strings';
 
-export type LanguageCode = 'en' | 'fr' | 'es';
+export type LanguageCode = 'en' | 'es' | 'fr' | 'pr';
 
 export type I18nValue = LiteralObject<any>;
 
